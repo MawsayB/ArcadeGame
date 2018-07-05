545d2e874424f17082351b17e499bb973c21c8eb
Marcy Bursac <msbursac@gmail.com>
2018-07-04 21:45:33 -0500

    displays gameboard!

--- js/engine.js
+++ js/engine.js
@@ -30,43 +30,43 @@ var Engine = (function(global) {
 
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
-    //  */
-    // function main() {
-    //     /* Get our time delta information which is required if your game
-    //      * requires smooth animation. Because everyone's computer processes
-    //      * instructions at different speeds we need a constant value that
-    //      * would be the same for everyone (regardless of how fast their
-    //      * computer is) - hurray time!
-    //      */
-    //     var now = Date.now(),
-    //         dt = (now - lastTime) / 1000.0;
+     */
+    function main() {
+        /* Get our time delta information which is required if your game
+         * requires smooth animation. Because everyone's computer processes
+         * instructions at different speeds we need a constant value that
+         * would be the same for everyone (regardless of how fast their
+         * computer is) - hurray time!
+         */
+        var now = Date.now(),
+            dt = (now - lastTime) / 1000.0;
 
-    //     /* Call our update/render functions, pass along the time delta to
-    //      * our update function since it may be used for smooth animation.
-    //      */
-    //     update(dt);
-    //     render();
+        /* Call our update/render functions, pass along the time delta to
+         * our update function since it may be used for smooth animation.
+         */
+        update(dt);
+        render();
 
-    //     /* Set our lastTime variable which is used to determine the time delta
-    //      * for the next time this function is called.
-    //      */
-    //     lastTime = now;
+        /* Set our lastTime variable which is used to determine the time delta
+         * for the next time this function is called.
+         */
+        lastTime = now;
 
-    //     /* Use the browser's requestAnimationFrame function to call this
-    //      * function again as soon as the browser is able to draw another frame.
-    //      */
-    //     win.requestAnimationFrame(main);
-    // }
+        /* Use the browser's requestAnimationFrame function to call this
+         * function again as soon as the browser is able to draw another frame.
+         */
+        win.requestAnimationFrame(main);
+    }
 
     /* This function does some initial setup that should only occur once,
      * particularly setting the lastTime variable that is required for the
      * game loop.
      */
-    // function init() {
-    //     reset();
-    //     lastTime = Date.now();
-    //     main();
-    // }
+    function init() {
+        reset();
+        lastTime = Date.now();
+        main();
+    }
 
     /* This function is called by main (our game loop) and itself calls all
      * of the functions which may need to update entity's data. Based on how
@@ -77,10 +77,10 @@ var Engine = (function(global) {
      * functionality this way (you could just implement collision detection
      * on the entities themselves within your app.js file).
      */
-    // function update(dt) {
-    //     updateEntities(dt);
-    //     // checkCollisions();
-    // }
+    function update(dt) {
+        updateEntities(dt);
+        // checkCollisions();
+    }
 
     /* This is called by the update function and loops through all of the
      * objects within your allEnemies array as defined in app.js and calls
@@ -89,12 +89,13 @@ var Engine = (function(global) {
      * the data/properties related to the object. Do your drawing in your
      * render methods.
      */
-    // function updateEntities(dt) {
-    //     allEnemies.forEach(function(enemy) {
-    //         enemy.update(dt);
-    //     });
-    //     player.update();
-    // }
+    function updateEntities(dt) {
+        /*
+        allEnemies.forEach(function(enemy) {
+            enemy.update(dt);
+        });
+        player.update();*/
+    }
 
     /* This function initially draws the "game level", it will then call
      * the renderEntities function. Remember, this function is called every
@@ -138,31 +139,31 @@ var Engine = (function(global) {
             }
         }
 
-        // renderEntities();
+        renderEntities();
     }
 
     /* This function is called by the render function and is called on each game
-    * on your enemy and player entities within app.js
      * tick. Its purpose is to then call the render functions you have defined
+     * on your enemy and player entities within app.js
      */
     function renderEntities() {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
-    //     allEnemies.forEach(function(enemy) {
-    //         enemy.render();
-    //     });
+        /*allEnemies.forEach(function(enemy) {
+            enemy.render();
+        });
 
-        player.render();
+        player.render();*/
     }
 
     /* This function does nothing but it could have been a good place to
      * handle game reset states - maybe a new game menu or a game over screen
      * those sorts of things. It's only called once by the init() method.
      */
-    // function reset() {
-    //     // noop
-    // }
+    function reset() {
+        // noop
+    }
 
     /* Go ahead and load all of the images we know we're going to need to
      * draw our game level. Then set init as the callback method, so that when
